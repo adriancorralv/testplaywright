1db3fd0bd1c679ec78c268a4335980a39214a161
Adrian Corral <sistemas@cedhsonora.org.mx>
2024-10-15 09:44:53 -0700

    cambios en el test para que sea más lento

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,10 +3,13 @@ const { test, expect } = require('@playwright/test');
 
 
 
-  test('Busca prroducto y da clic', async ({page})=>{
+  test('Busca producto y da clic', async ({page})=>{
     await page.goto('https://www.amazon.com.mx/');
+  
+  await expect(page.locator('[id=twotabsearchtextbox]')).toBeVisible({ timeout: 10000});
     await page.locator('[id=twotabsearchtextbox]').fill('PS5');
     await page.locator('[id=nav-search-submit-button]').click();
+    await expect(page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]')).toBeVisible({ timeout: 10000});
     await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').first().click();
 
 
