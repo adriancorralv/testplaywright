d005ecde81cb252045bf5bb9d9ba05cf72153e40
Adrian Corral <sistemas@cedhsonora.org.mx>
2024-10-15 17:35:21 -0700

    chang ets

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,16 +6,18 @@ const { test, expect } = require('@playwright/test');
 test('Busca producto y da clic', async ({ page }) => {
   console.log("Navegando a Amazon...");
   await page.goto('https://www.amazon.com.mx/');
-  console.log("Esperando que el campo de búsqueda sea visible...");
-  await page.locator('[id=twotabsearchtextbox]');
-  console.log("Llenando el campo de búsqueda...");
-  await page.locator('[id=twotabsearchtextbox]').fill('PS5');
-  console.log("Haciendo clic en el botón de búsqueda...");
+  await page.locator('[id=navbar-main]').isVisible();
   await page.locator('[id=nav-search-submit-button]').click();
-  console.log("Esperando resultados...");
-  await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]');
-  console.log("Haciendo clic en el primer resultado...");
-  await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').first().click();
-  console.log("Prueba completada");
+  // console.log("Esperando que el campo de búsqueda sea visible...");
+  // await page.locator('[id=twotabsearchtextbox]');
+  // console.log("Llenando el campo de búsqueda...");
+  // await page.locator('[id=twotabsearchtextbox]').fill('PS5');
+  // console.log("Haciendo clic en el botón de búsqueda...");
+  //id="nav-search-submit-button"  // await page.locator('[id=nav-search-submit-button]').click();
+  // console.log("Esperando resultados...");
+  // await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]');
+  // console.log("Haciendo clic en el primer resultado...");
+  // await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').first().click();
+  // console.log("Prueba completada");
 });
 
