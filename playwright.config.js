ebbd6dfcd52be203d2681181b1b41f817720510f
Adrian Corral <sistemas@cedhsonora.org.mx>
2024-10-15 18:16:44 -0700

    change allur

--- playwright.config.js
+++ playwright.config.js
@@ -22,8 +22,8 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["line"], ["allure-playwright"]],
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters ["allure-playwright"]], */
+  reporter: [["line"]], 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
