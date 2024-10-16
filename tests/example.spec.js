6ca7f63dc206c1442bc23b2964cb3666f271483c
Adrian Corral <sistemas@cedhsonora.org.mx>
2024-10-15 17:44:10 -0700

    wait

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,14 +6,17 @@ const { test, expect } = require('@playwright/test');
 test('Busca producto y da clic', async ({ page }) => {
   console.log("Navegando a Amazon...");
   await page.goto('https://www.amazon.com.mx/');
+  await page.waitForTimeout(2000);
   await page.locator('[id=navbar-main]').isVisible();
 //  await page.locator('[id=nav-search-submit-button]').click();
   // console.log("Esperando que el campo de búsqueda sea visible...");
    await page.locator('[id=twotabsearchtextbox]').click();
   // console.log("Llenando el campo de búsqueda...");
  await page.locator('[id=twotabsearchtextbox]').fill('PS5');
+ await page.waitForTimeout(2000);
   // console.log("Haciendo clic en el botón de búsqueda...");
    await page.locator('[id=nav-search-submit-button]').click();
+   await page.waitForTimeout(2000);
   // console.log("Esperando resultados...");
   //await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]');
   // console.log("Haciendo clic en el primer resultado...");
