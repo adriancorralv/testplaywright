928a6ca38c76d826eddca853d54c302be6203fb0
Adrian Corral <sistemas@cedhsonora.org.mx>
2024-10-15 17:40:58 -0700

    hf

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,17 +7,17 @@ test('Busca producto y da clic', async ({ page }) => {
   console.log("Navegando a Amazon...");
   await page.goto('https://www.amazon.com.mx/');
   await page.locator('[id=navbar-main]').isVisible();
-  await page.locator('[id=nav-search-submit-button]').click();
+//  await page.locator('[id=nav-search-submit-button]').click();
   // console.log("Esperando que el campo de búsqueda sea visible...");
-  // await page.locator('[id=twotabsearchtextbox]');
+   await page.locator('[id=twotabsearchtextbox]').click();
   // console.log("Llenando el campo de búsqueda...");
-  // await page.locator('[id=twotabsearchtextbox]').fill('PS5');
+ await page.locator('[id=twotabsearchtextbox]').fill('PS5');
   // console.log("Haciendo clic en el botón de búsqueda...");
-  //id="nav-search-submit-button"  // await page.locator('[id=nav-search-submit-button]').click();
+   await page.locator('[id=nav-search-submit-button]').click();
   // console.log("Esperando resultados...");
-  // await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]');
+  //await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]');
   // console.log("Haciendo clic en el primer resultado...");
-  // await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').first().click();
+   await page.locator('a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').first().click();
   // console.log("Prueba completada");
 });
 
